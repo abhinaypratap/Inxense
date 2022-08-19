eaee61a2ba494a06b4574167dafb94b79bfe2119
Abhinay Pratap <abhinaypratap@icloud.com>
2022-08-19 19:00:09 +0530

    create project

---
+++ Inxense/InxenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  InxenseApp.swift
+//  Inxense
+//
+//  Created by Abhinay Pratap on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct InxenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Inxense Login Screen
+
